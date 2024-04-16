7ba035fa5f7ea13525a9d67286b71561ef24fc08
antu2809 <FranciscoAntuAlmonacid@4146c987a9a6>
2024-04-16 16:15:10 +0000

    Actualizar aplicación con nuevas configuraciones desde PythonAnywhere

--- app/portfolio.py
+++ app/portfolio.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, send_from_directory
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Email
-from flask_mail import Mail, Message 
+from flask_mail import Mail, Message
 from dotenv import load_dotenv
 import os
 load_dotenv()
@@ -10,39 +10,29 @@ load_dotenv()
 app = Flask(__name__, static_url_path='/static')
 app.config['SECRET_KEY'] = 'PorfolioAlmonacidCammarata111200'
 
-# Configuración de Flask-Mail
-app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
-app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT'))
-app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS') == 'True'
-app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-
-
-mail = Mail(app)
-
 class ContactForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     message = TextAreaField('Message', validators=[DataRequired()])
     submit = SubmitField('Send')
-    
-    
+
+
 @app.route('/error')
 def error():
     form = ContactForm()
-    return render_template('error.html', message="Bad Request", form=form) 
+    return render_template('error.html', message="Bad Request", form=form)
 
 
 @app.errorhandler(Exception)
 def handle_exception(e):
     form = ContactForm()
-    return render_template('error.html', message="Bad Request", form=form), 400  
+    return render_template('error.html', message="Bad Request", form=form), 400
 
 
 @app.errorhandler(404)
 def err_404(e):
     form = ContactForm()
-    return render_template('error.html', message='404 Page Not Found', form=form), 404  
+    return render_template('error.html', message='404 Page Not Found', form=form), 404
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -52,55 +42,16 @@ def index():
     tech = "Python (Django and Flask), HTML, CSS, Javascript, Bootstrap, Git (GitHub, GitLab)"
     loves = "Writing; Sharing knowledge with interested persons; Sports; Music Production"
 
-    experiences = [
-        {
-            'date': 'March - June 2023',
-            'title': 'Website Development',
-            'company': 'La galería Rosa',
-            'description': 'My first project. A platform allowing users to showcase, acquire, and exchange available artistic creations through an interactive web interface.',
-            'skills': [
-                'Front End: HTML, CSS and Javascript',
-                'Back-End: Python 3.2.0; Django 4.2.1'
-            ],
-            
-            'gif1_path': 'img/lagaleriarosapresentacion.gif',
-            'gif2_path': 'img/lagaleriarosahome.gif'
-            
-        },
-        {
-            'date': 'July 2023',
-            'title': 'E-commerce Website Development',
-            'company': 'New Friends  ',
-            'description': 'A sophisticated online platform designed as a virtual retail hub, allowing the New Friends brand to showcase and exhibit its extensive inventory of merchandise, thereby paving the way for seamless and streamlined sales transactions.',
-            'skills': [
-                'Front End: HTML, CSS and Javascript',
-                'Back-End: Python 3.2.0; Django 4.2.1'
-            ],
-            "image_path": "img/newfriends.png"
-        },
-        {
-            'date': 'August 2023',
-            'title': '@trenzas.laplata Website Developer',
-            'company': 'Trenzas La Plata',
-            'description': 'Online catalog that allows the brand @trenza.laplata to share the price list of its different services.',
-            'skills': [
-                'Front End: HTML, CSS and Javascript',
-                'Back-End: Python 3.2.0; Flask'
-            ],
-            'gif1_path': 'img/trenzas_laplata.gif'
-        }
-    ]
-
     educations = [
         {'date': '2012 / 2018',
             'degree': 'Technical degree in agricultural production corresponding to completed secondary education',
             'institution': 'Secondary School of Technical and Professional Education No. 733 "Benito Owen"',
-            'location': 'Gaiman, Chubut, Argentina'    
+            'location': 'Gaiman, Chubut, Argentina'
         },
         {'date': 'July 2017',
             'degree': 'English',
             'institution': 'Gateway School of English',
-            'location': 'La Valeta, Malta'    
+            'location': 'La Valeta, Malta'
         },
         {
             'date': '2019 / Present',
@@ -118,36 +69,33 @@ def index():
     form = ContactForm()
 
     if form.validate_on_submit():
-        msg = Message('Nuevo mensaje de contacto', sender='tu_correo@gmail.com', recipients=['antualmonacid@gmail.com'])
+        msg = Message('Nuevo mensaje de contacto', sender=form.email.data, recipients=['antu.almonacid@alu.ing.unlp.edu.ar'])
         msg.body = f"Nombre: {form.name.data}\nEmail: {form.email.data}\nMensaje: {form.message.data}"
         mail.send(msg)
         return redirect(url_for('index'))
 
     return render_template('index.html', name=name, status=status, fields=fields, tech=tech, loves=loves, experiences=experiences, educations=educations, form=form)
 
+# Ruta para servir el archivo PDF del currículum
+@app.route('/get_cv_pdf')
+def get_cv_pdf():
+    return send_from_directory('static', 'Almonacid.Francisco.Developer.pdf')
 
-
-@app.route('/experience')
-def experience():
-    experiences = [
+experiences = [
         {
-            'date': 'March - June 2023',
-            'title': 'Website Developer',
-            'company': 'La galería Rosa',
-            'description': 'My first project. A platform allowing users to showcase, acquire, and exchange available artistic creations through an interactive web interface.',
-            'skills': [
-                'Front End: HTML, CSS and Javascript',
-                'Back-End: Python 3.2.0; Django 4.2.1'
-            ],
-            'gif1_path': 'img/lagaleriarosapresentacion.gif',
-            'gif2_path': 'img/lagaleriarosahome.gif'
-            
-        },
-        {
+            'id': 1,
             'date': 'July 2023',
-            'title': 'Website Developer',
-            'company': 'New Friends  ',
-            'description': 'A sophisticated online platform designed as a virtual retail hub, allowing the New Friends brand to showcase and exhibit its extensive inventory of merchandise, thereby paving the way for seamless and streamlined sales transactions.',
+            'title': 'Antu Shop',
+            'repository': 'https://github.com/antu2809/antushop',
+            'website': 'https://antudev.pythonanywhere.com/',
+            'company': '@antu.beats',
+            'description_part1': 'A sophisticated online platform designed as a virtual retail hub, allowing producer @antu.beats to display and showcase his extensive inventory of instrumentals.',
+            'description_part2': 'In the backend configuration, I addressed the management of authentication, registration, password change, and payments in Django, integrating MercadoPago API configuration for secure and efficient payment processing. I addressed model management to allow the publication of different products. Furthermore, I configured the cart for its proper functioning in the store.',
+            'description_part3': 'On the frontend I created an intuitive design, secure access, easy registration, interactive store with integrated payment experience using html5 and css3.',
+            'description': 'A sophisticated online platform designed as a virtual retail hub, allowing producer @antu.beats to display and showcase his extensive inventory of instrumentals.',
+            'image1_path': 'img/tienda1.png',
+            'image2_path': 'img/tienda2.png',
+            'image3_path': 'img/tienda3.png',
             'skills': [
                 'Front End: HTML, CSS and Javascript',
                 'Back-End: Python 3.2.0; Django 4.2.1'
@@ -155,20 +103,72 @@ def experience():
             "image_path": "img/newfriends.png"
         },
         {
+            'id': 2,
             'date': 'August 2023',
-            'title': 'Website Developer',
+            'title': '@trenzas.laplata Online Catlog',
+            'repository': 'https://github.com/antudev/trenzas-laplata',
+            'website': 'https://trenzaslaplata.onrender.com/',
             'company': 'Trenzas La Plata',
-            'description': 'Online catalog that allows the brand @trenza.laplata to share the price list of its different services.',
+            'description_part1': 'Online catalog that allows the brand @trenzas.laplata to share the price list of its different services and share valuable information about their work',
+            'description_part2': 'In FLask I configured a postgresql database connection, defined braiding and price models, routes to display prices and braiding types, and a chatbot function to answer questions.',
+            'description_part3': 'The frontend uses HTML, CSS and JavaScript to create a responsive web page with a drop-down menu, informational sections and an integrated chatbot. ',
+            'description':  'Online catalog that allows the brand @trenza.laplata to share the price list of its different services and share valuable information about their work',
+            'image1_path': 'img/trenzas (1).png',
+            'image2_path': 'img/trenzas (2).png',
+            'image3_path': 'img/trenzas (3).png',
             'skills': [
                 'Front End: HTML, CSS and Javascript',
                 'Back-End: Python 3.2.0; Flask'
             ],
             'gif1_path': 'img/trenzas_laplata.gif',
+            'gif2_path': 'img/trenzas_laplata_mobile[1].gif'
+        },
+        {
+            'id': 3,
+            'date': 'March - June 2023',
+            'title': 'La galería rosa',
+            'repository': 'https://github.com/antudev/trenzas-laplata',
+            'website': 'not yet',
+            'company': 'La galería rosa',
+            'description_part1': 'A platform allowing users to showcase, acquire, and exchange available artistic creations through an interactive web interface.',
+            'description_part2': 'The backend provides functions for adding customers, managing an online store, processing purchases, displaying images, and getting information from Instagram.',
+            'description_part3': 'The frontend features an elegant interface with personalized greetings, an art gallery, store and social links, and search functionalities.',
+            'description': 'My first project. A platform allowing users to showcase, acquire, and exchange available artistic creations through an interactive web interface.',
+            'image1_path': 'img/lagaleriarosapresentacion.gif',
+            'image2_path': 'img/lagaleriarosahome.gif',
+            'image3_path': 'img/galeria.png',
+            'skills': [
+                'Front End: HTML, CSS and Javascript',
+                'Back-End: Python 3.2.0; Django 4.2.1'
+            ],
+
+            'gif1_path': 'img/lagaleriarosapresentacion.gif',
+            'gif2_path': 'img/lagaleriarosahome.gif'
+
         }
     ]
 
+@app.route('/experience')
+def experience():
+
+
     return render_template('experience.html', experiences=experiences)
 
+@app.route('/project/<int:project_id>')
+def project_details(project_id):
+    # Lógica para obtener los detalles del proyecto con el ID especificado
+    # Se asume que tienes una lista de proyectos llamada "experiences"
+    project = None
+    for experience in experiences:
+        if experience['id'] == project_id:
+            project = experience
+            break
+
+    if project is None:
+        # Manejar el caso en que no se encuentre el proyecto con el ID especificado
+        return "Proyecto no encontrado", 404
+
+    return render_template('project_details.html', project=project)
 
 @app.route('/education')
 def education():
@@ -197,8 +197,8 @@ def contact():
     if form.validate_on_submit():
         # Envío de correo electrónico
         msg = Message('Nuevo mensaje de contacto',  # Asunto del correo
-                      sender='tu_correo@gmail.com',  # Cambia a tu dirección de correo
-                      recipients=['antualmonacid@gmail.com'])  # Tu dirección de correo destinatario
+                      sender=form.email.data, # Correo elecctronico del remitente
+                      recipients=['antu.almonacid@alu.ing.unlp.edu.ar'])  # Tu dirección de correo destinatario
         msg.body = f"Nombre: {form.name.data}\nEmail: {form.email.data}\nMensaje: {form.message.data}"
         mail.send(msg)
 
--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,215 +1,76 @@
-function toggleMenu() {
-    var menu = document.getElementById("nav-menu");
-    var burger = document.querySelector(".burger");
-    var body = document.querySelector("body");
+document.addEventListener('DOMContentLoaded', function() {
+    const menuButton = document.querySelector('.button-menu');
+    const menuContainer = document.getElementById('menu-container');
+    const toggleSwitch = document.querySelector('.toggle-switch input[type="checkbox"]');
 
-    menu.classList.toggle("show");
-    burger.classList.toggle("burger-active");
-    body.classList.toggle("no-scroll");
-}
-
-function switchTheme() {
-  var body = document.getElementsByTagName('body')[0];
-  var checkbox = document.getElementsByTagName('input')[0];
-  var header = document.getElementById('page-header');
-  var skillsSection = document.querySelector('.special-skills-container');
-  var experienceSection = document.querySelector('.experience-container');
-
-  if (checkbox.checked) {
-      body.setAttribute('data-theme', 'dark');
-  } else {
-      body.setAttribute('data-theme', 'light');
-  }
-}
-
-// Función para inicializar las animaciones
-function initAnimations() {
-  const clipPathContainer = document.querySelector('.clip-path-container');
-  clipPathContainer.classList.add('animate');
-}
-
-// Llama a initAnimations cuando se carga la página y en el evento resize si el ancho es menor o igual a 768px
-window.addEventListener('load', () => {
-  initAnimations();
-});
-
-window.addEventListener('resize', () => {
-  if (window.innerWidth <= 768) {
-    initAnimations();
-  }
-});
-
-window.addEventListener('load', function() {
-    const clipPathContainer = document.querySelector('.clip-path-container');
-    clipPathContainer.classList.add('animate');
-   });
-
-document.addEventListener("DOMContentLoaded", function() {
-  // Obtenemos los elementos de los logos de las redes sociales
-  const linkedinLogo = document.querySelector(".social-link[data-profile='linkedin']");
-  const githubLogo = document.querySelector(".social-link[data-profile='github']");
-  const socialLinks = document.querySelectorAll(".social-link");
-  
-  // Eventos de escucha para los eventos 'mouseover', 'mouseout', 'touchstart' y 'touchend' de los logos
-  linkedinLogo.addEventListener("mouseover", showProfileTooltip);
-  linkedinLogo.addEventListener("mouseout", hideProfileTooltip);
-
-  githubLogo.addEventListener("mouseover", showProfileTooltip);
-  githubLogo.addEventListener("mouseout", hideProfileTooltip);
-
-   // Función para mostrar el tooltip del perfil
-   function showProfileTooltip(event) {
-    // Obtenemos el enlace padre del elemento img
-    const socialLink = event.currentTarget.closest(".social-link");
-  
-    // Obtenemos la red social correspondiente y la información del perfil
-    const social = socialLink.getAttribute("data-profile");
-    const profileData = getProfileData(social);
-  
-    // Creamos el tooltip y establecemos su contenido
-    const tooltip = document.createElement("div");
-    tooltip.classList.add("profile-tooltip");
-    tooltip.innerHTML = `
-      <img src="${profileData.photo}" alt="${social} profile photo">
-      <p>${profileData.info}</p>
-    `;
-  
-    // Establecemos la posición del tooltip debajo del logo de la red social
-    const logoRect = socialLink.getBoundingClientRect();
-    tooltip.style.top = `${logoRect.bottom}px`;
-    tooltip.style.left = `${logoRect.left}px`;
-  
-    // Agregamos el tooltip al documento
-    document.body.appendChild(tooltip);
-  }
-  
-  // Función para ocultar el tooltip del perfil
-  function hideProfileTooltip(event) {
-    const tooltip = document.querySelector(".profile-tooltip");
-    if (tooltip) {
-      document.body.removeChild(tooltip);
-    }
-  }
-  
-  // Función para obtener la información del perfil según la red social
-  function getProfileData(social) {
-    const profiles = {
-      linkedin: {
-        photo: "https://media.licdn.com/dms/image/D4D35AQGWTLcQ5UjJ_A/profile-framedphoto-shrink_400_400/0/1687451409358?e=1688115600&v=beta&t=MXu6lO08hessKpoOpFQ5lizh4OeBesQtHztOPDtNCZE",
-        info: "https://www.linkedin.com/in/francisco-antu-almonacid-cammarata-543119267/opportunities/job-opportunities/details?profileUrn=urn%3Ali%3Afs_normalized_profile%3AACoAAEFVzMMBP0xOzrB1QPHq4oE2lDHYeBhaU84&trk=opento_sprofile_details&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3B4EC%2FuIPlR%2Fei3Br%2BxhBovA%3D%3D"
-      },
-      github: {
-        photo: "https://avatars.githubusercontent.com/u/102919566?v=4",
-        info: "https://github.com/antu2809?tab=repositories"
-      }
-    };
-  
-    return profiles[social];
-  }
-
-  // Agregar evento de clic a cada logo en dispositivos moviles
-  socialLinks.forEach(socialLink => {
-    socialLink.addEventListener("click", handleSocialLinkClick);
-  });
-
-  // Función para manejar el clic en los logos en dispositivos moviles
-  function handleSocialLinkClick(event) {
-    const socialLink = event.currentTarget;
-    const profile = socialLink.getAttribute("data-profile");
-    const profileData = getProfileData(profile);
-
-    const userWantsToRedirect = confirm(`Do you want to visit ${profile} page?`);
-    
-    if (userWantsToRedirect) {
-      window.location.href = profileData.info;
-    }
-  }
-
-
-// Obtener todos los elementos del acordeón
-const accordionItems = document.querySelectorAll('.accordionItem');
-
-  accordionItems.forEach((item) => {
-    const title = item.querySelector('.accordionItem_title');
-    const content = item.querySelector('.accordionItem_content');
-
-    title.addEventListener('click', () => {
-      content.classList.toggle('collapsed');
-      title.classList.toggle('active');
-
-      if (content.classList.contains('collapsed')) {
-        content.style.maxHeight = null;
-      } else {
-        content.style.maxHeight = content.scrollHeight + 'px';
-      }
+    menuButton.addEventListener('click', function() {
+        console.log("Menu button clicked.");
+        menuContainer.classList.toggle('active');
     });
 
-    content.style.maxHeight = 0;
-    content.classList.add('collapsed');
-  });
-  const educationCarousel = $(".education-carousel");
-  if (educationCarousel.length) {
-    initializeSlickCarousel(educationCarousel);
-  }
+    toggleSwitch.addEventListener('change', switchTheme);
 
-  // Función para agregar la funcionalidad del acordeón
-  function toggleAccordion() {
-    const accordionTitles = document.querySelectorAll('.accordionItem_title');
-    const accordionContents = document.querySelectorAll('.accordionItem_content');
-    
-    accordionTitles.forEach((title) => {
-      title.addEventListener('click', () => {
-        const content = title.nextElementSibling;
-        
-        title.classList.toggle('active');
-        content.classList.toggle('collapsed');
-        
-        if (content.classList.contains('collapsed')) {
-          content.style.maxHeight = null;
+    document.getElementById('menu-button').addEventListener('click', function() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth' // Hace que el desplazamiento sea suave
+        });
+    });
+
+    function switchTheme() {
+        var body = document.getElementsByTagName('body')[0];
+        var checkbox = document.querySelector('.toggle-switch input[type="checkbox"]');
+        var header = document.getElementById('page-header');
+        var skillsSection = document.querySelector('.special-skills-container');
+        var experienceSection = document.querySelector('.experience-container');
+        var navLinks = document.querySelectorAll('.nav-d a');
+
+        if (checkbox.checked) {
+            body.setAttribute('data-theme', 'dark');
         } else {
-          content.style.maxHeight = content.scrollHeight + 'px';
+            body.setAttribute('data-theme', 'light');
         }
-      });
+    }
+
+
+    var lastScrollTop = 0;
+
+    window.addEventListener('scroll', function() {
+        var menuContainer = document.getElementById('menu-container');
+        var currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (currentScrollTop > lastScrollTop) { // Si el usuario se desplaza hacia abajo
+            menuContainer.classList.remove('active'); // Quita la clase 'active'
+        }
+
+        lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop; // Para manejar el desplazamiento hacia arriba en la parte superior de la página
     });
 
-    accordionContents.forEach((content) => {
-      content.style.maxHeight = 0;
-      content.classList.add('collapsed');
-    });
-  }
 
-  // Llama a la función para agregar la funcionalidad del acordeón
-  toggleAccordion();
+     // Función ScrollReveal().reveal()
+    ScrollReveal().reveal('.dual__content', {
+        // Opciones de animación (puedes ajustarlas según tus necesidades)
+        origin: 'left', // La animación comenzará desde la izquierda
+        distance: '50px', // La distancia de desplazamiento es de 50px
+        duration: 1000, // Duración de la animación en milisegundos (1 segundo)
+        delay: 0 // No hay retraso antes de que comience la animación
+    });
+    ScrollReveal().reveal('.dual__inner', {
+        // Opciones de animación (puedes ajustarlas según tus necesidades)
+        origin: 'bottom', // La animación comenzará desde abajo
+        distance: '50px', // La distancia de desplazamiento es de 50px
+        duration: 1000, // Duración de la animación en milisegundos (1 segundo)
+        delay: 0 // No hay retraso antes de que comience la animación
+    });
+
+     // Función ScrollReveal().reveal()
+    ScrollReveal().reveal('.content__title--left', {
+        // Opciones de animación (puedes ajustarlas según tus necesidades)
+        origin: 'bottom', // La animación comenzará desde la izquierda
+        distance: '50px', // La distancia de desplazamiento es de 50px
+        duration: 1000, // Duración de la animación en milisegundos (1 segundo)
+        delay: 0 // No hay retraso antes de que comience la animación
+    });
 });
-
-// main.js
-function toggleContent() {
-  const experienceList = document.querySelector('.experience-list');
-  const accordion = document.querySelector('.ohio-accordion-sс');
-
-  if (window.innerWidth <= 768) {
-    experienceList.style.display = 'block';
-    accordion.style.display = 'none';
-  } else {
-    experienceList.style.display = 'none';
-    accordion.style.display = 'block';
-  }
-}
-
-// Ejecutar al cargar la página y cuando se cambie el tamaño de la ventana
-toggleContent();
-window.addEventListener('resize', toggleContent);
-
-function initializeSlickCarousel(carouselElement) {
-  carouselElement.slick({
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
-    // Otras opciones adicionales
-  });
-}
-
 
 
